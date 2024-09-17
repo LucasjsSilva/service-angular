718b42bfcbee5c131cb7cb3bcf6b0d356f104fec
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2024-09-17 12:17:24 -0300

    finalizado

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -6,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  name:string = "bulbasour";
-  atributesTypes:string[] = ['venenoso', 'planta'];
-  constructor() { }
+  pokemon:PokemonData
+
+  constructor(
+    private service:PokemonService
+  ) {
+    this.pokemon= {
+      id: 0,
+      name: '',
+      sprites: {front_default: ''},
+      types: []
+    }
+  }
 
   ngOnInit(): void {
+    this.getPokemon("pikachu")
+  }
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+        },
+        error: (err) => console.log("not found")
+      }
+    )
   }
 
 }
